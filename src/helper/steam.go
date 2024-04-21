4d57bd5d34f067fe5225b7fe3341d6aedf1ffe13
Leo Developer <leodeveloper@protonmail.com>
2024-04-21 12:21:39 +0200

    various steam start fixes

--- src/helper/steam.go
+++ src/helper/steam.go
@@ -95,13 +95,23 @@ func findSteamProcess() (*process.Process, error) {
 }
 
 func RunSteamWithArguments(options *options.Options, args ...string) error {
-	executable, err := FindSteamExecutable(options)
-	if err != nil {
-		return err
+	var executable string
+	var err error
+
+	if runtime.GOOS == "linux" {
+		// steam is a shell script on linux, so we need to run it with bash
+		executable = "/bin/bash"
+		args = append([]string{"-c", "steam"}, args...)
+	} else {
+		executable, err = FindSteamExecutable(options)
+		if err != nil {
+			return err
+		}
 	}
 	if options.SteamDev {
 		args = append(args, "-dev")
 	}
+
 	if options.Verbose {
 		fmt.Println("Running Steam with arguments:", executable, args)
 	}
@@ -109,7 +119,15 @@ func RunSteamWithArguments(options *options.Options, args ...string) error {
 	// steam is dying without having stdout attached
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Start()
+
+	if runtime.GOOS == "windows" {
+		// so windows apparently doesnt have the bootstrapper and it will directly start steam
+		// and if we ran .Run() it would block the process until steam is closed, which is not what we want
+		cmd.Start()
+	} else {
+		// this will run the bootstrapper and then block until its done and starts the actual steam process
+		cmd.Run()
+	}
 	return nil
 }
 
