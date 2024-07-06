9b3a5c709754b0322e46affb4c03e8d5b59c11ea
Leo Developer <leodeveloper@protonmail.com>
2024-07-06 21:48:15 +0200

    open returns an error if the url is invalid now

--- src/bindings/os-native.go
+++ src/bindings/os-native.go
@@ -8,17 +8,18 @@ import (
 	"github.com/sag-enhanced/native-app/src/helper"
 )
 
-func (b *Bindings) Open(target string) {
+func (b *Bindings) Open(target string) error {
 	// some sanization checks
 	if strings.ContainsAny(target, "\n\r'\"` {}$|;") {
-		return
+		return fmt.Errorf("Invalid URL")
 	}
 	url, err := url.Parse(target)
 	// only allow https urls and block any path traversal attempts
 	if err != nil || url.Scheme != "https" || strings.Contains(url.Path, "..") {
-		return
+		return fmt.Errorf("Invalid URL")
 	}
 	fmt.Println("Opening URL", url.String())
 	// re-assemble url to string to avoid any funny business
 	helper.Open(url.String(), b.options)
+  return nil
 }
